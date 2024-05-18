5ea1f14f63e87898000608e6fe8e5042d5554be7
Yeakub1 <mryeakub10@gmail.com>
2024-05-18 23:54:02 +0600

    code update

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,11 @@
-export interface Blog{
-    id: string;
-    title: string;
-    description: string;
-    published_date: string;
-    author_id: string
-    blog_image: string
-    total_likes: string
+export interface Blog {
+  id: string;
+  title: string;
+  description: string;
+  published_date: string;
+  author_id: string;
+  blog_image: string;
+  total_likes: string;
+  publish_date: string;
+  author_name: string;
 }
